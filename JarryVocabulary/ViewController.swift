559c279dd3dd25eb7c6de2ba1c7bb745b0ce9d16
cjyang <>
2020-07-07 23:29:07 +0900

    init project

---
+++ JarryVocabulary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JarryVocabulary
+//
+//  Created by cjyang on 2020/07/07.
+//  Copyright © 2020 jarry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
